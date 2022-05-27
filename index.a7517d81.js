df9fe182939feb3dc848b7bd5d9490298e857dbc
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-05-27 23:08:18 +0000

    Deploying to gh-pages from @ wi2410/donut-masterclass@95c6d1b82d6a4c9536b3f93f0a832a055018b5e7 🚀

--- index.a7517d81.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),o=document.querySelector("[data-body]");e.addEventListener("click",(function(){var a="true"===e.getAttribute("aria-expanded")||!1;e.setAttribute("aria-expanded",!a),t.classList.toggle("mobile-menu--is-open"),o.classList.toggle("body--scroll-hidden"),o.classList.toggle("body--scroll-hidden::-webkit-scrollbar")}))}();
-//# sourceMappingURL=index.a7517d81.js.map
