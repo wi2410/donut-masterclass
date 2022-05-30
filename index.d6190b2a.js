48abfdac37dca4e5d3aba1463979a8365c8daa30
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-05-30 16:34:16 +0000

    Deploying to gh-pages from @ wi2410/donut-masterclass@6dff9defba972449186a08b68374bfc42bac1149 🚀

---
+++ index.d6190b2a.js
@@ -0,0 +1,2 @@
+var readMoreBtn=document.querySelectorAll("[data-read-more]");readMoreBtn.forEach((function(e){e.addEventListener("click",(function(e){var t=e.target,n=t.previousElementSibling;n.classList.contains("is-hidden")?t.textContent="Less":t.textContent="Read more",n.classList.toggle("is-hidden")}))}));
+//# sourceMappingURL=index.d6190b2a.js.map
