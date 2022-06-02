eddb044839894032ae9e54a10bb2a743c527e882
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-06-02 18:54:27 +0000

    Deploying to gh-pages from @ wi2410/donut-masterclass@f8c89829e9e742ebd07b34326a78980a7f74c8c1 🚀

--- index.4b725516.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),o=document.querySelector("[data-body]");e.addEventListener("click",(()=>{const d="true"===e.getAttribute("aria-expanded")||!1;e.setAttribute("aria-expanded",!d),t.classList.toggle("mobile-menu--is-open"),document.body.classList.toggle("menu-open"),o.classList.toggle("body--scroll-hidden"),o.classList.toggle("body--scroll-hidden::-webkit-scrollbar")}))})();
-//# sourceMappingURL=index.4b725516.js.map
---
+++ index.cd986c78.js
@@ -0,0 +1,2 @@
+document.querySelectorAll('a[href^="#"]').forEach((function(e){e.addEventListener("click",(function(e){e.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})}))}));
+//# sourceMappingURL=index.cd986c78.js.map
