3c0385b76cbd86713ea7d5564f90d3fb9973ff1e
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-06-02 15:54:15 +0000

    Deploying to gh-pages from @ wi2410/donut-masterclass@f7cbd689a776d5b1f40292d7d75fc5e5b96b3ecc 🚀

--- index.d6190b2a.js
+++
@@ -1,2 +0,0 @@
-var readMoreBtn=document.querySelectorAll("[data-read-more]");readMoreBtn.forEach((function(e){e.addEventListener("click",(function(e){var t=e.target,n=t.previousElementSibling;n.classList.contains("is-hidden")?t.textContent="Less":t.textContent="Read more",n.classList.toggle("is-hidden")}))}));
-//# sourceMappingURL=index.d6190b2a.js.map
